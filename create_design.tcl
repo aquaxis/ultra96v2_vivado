187fa1ed292d4ae7835de1fe1bf9cc4979a07d64
Hidemi Ishihara <hidemi@aquaxis.com>
2020-06-10 07:51:29 +0900

    Update for Vivado/Vitis v2020.1

--- create_design.tcl
+++ create_design.tcl
@@ -2,14 +2,13 @@
 # Create Top Project
 
 set project_name "ultra96v2"
-set part_name "xczu3eg-sbva484-1-e"
+set part_name    "xczu3eg-sbva484-1-e"
+set block_name   "system"
 
 exec rm -rf ${project_name}
 
 # Create Project
 create_project ${project_name} ./${project_name} -part ${part_name}
-
-set block_name "system"
 
 # Build block design
 source src/${block_name}.tcl
@@ -23,28 +22,21 @@ foreach {ips} [lsort -decreasing $listips] {
 }
 
 # set parameter for clock
-#set_property pfm_name system [get_files {${project_name}/${project_name}.srcs/sources_1/bd/${block_name}/${block_name}.bd}]
-set_property PFM.CLOCK {pl_clk0 {id "0" is_default "true" proc_sys_reset "/proc_sys_reset_0" status "fixed"}} [get_bd_cells /zynq_ultra_ps_e_0]
+#set_property PFM.CLOCK {pl_clk0 {id "0" is_default "true" proc_sys_reset "/proc_sys_reset_0" status "fixed"}} [get_bd_cells /zynq_ultra_ps_e_0]
 
 # make wrapper
 make_wrapper -files [get_files ${project_name}/${project_name}.srcs/sources_1/bd/${block_name}/${block_name}.bd] -top
 read_verilog ${project_name}/${project_name}.srcs/sources_1/bd/${block_name}/hdl/${block_name}_wrapper.v
-update_compile_order -fileset sources_1
 
-# Close Project
-close_project
-
-# open project
-open_project ${project_name}/${project_name}.xpr
-
-# set pfn_name
-open_bd_design ${project_name}/${project_name}.srcs/sources_1/bd/${block_name}/${block_name}.bd
-set_property pfm_name system [get_files ${project_name}/${project_name}.srcs/sources_1/bd/${block_name}/${block_name}.bd]
-set_property PFM.CLOCK {pl_clk0 {id "0" is_default "true" proc_sys_reset "/proc_sys_reset_0" status "fixed"}} [get_bd_cells /zynq_ultra_ps_e_0]
+# set top
+set_property top ${block_name}_wrapper  [current_fileset]
 
 # Synthesis
 #set_param general.maxThreads 4
 #synth_design -name ${project_name} -top ${block_name}_wrapper -part ${part_name} -mode out_of_context
+
+# set pfm_name
+set_property pfm_name system [get_files ${project_name}/${project_name}.srcs/sources_1/bd/${block_name}/${block_name}.bd]
 
 # Export Hardware
 write_hw_platform -force  -file ${project_name}.xsa
--- create_pfm.tcl
+++ create_pfm.tcl
@@ -1,8 +1,11 @@
+set yocto_path "/home/hidemi/yocto/yocto_dunfell/build_ultra96v2"
+set sdk_path   "/opt/poky/3.1_ultra96v2"
+
 platform create -name "ultra96v2_platform" -hw ./ultra96v2.xsa -out ./ -no-boot-bsp -proc psu_cortexa53 -os linux
 
 domain active linux_domain
-domain config -bif /home/hidemi/yocto/yocto_dunfell/build_ultra96v2/sd_card/build_vitis.bif
-domain config -boot /home/hidemi/yocto/yocto_dunfell/build_ultra96v2/sd_card
-domain config -image /home/hidemi/yocto/yocto_dunfell/build_ultra96v2/sd_card
-domain config -sysroot /opt/poky/3.1_ultra96v2/sysroots/aarch64-poky-linux
+domain config -bif ${yocto_path}/sd_card/build_vitis.bif
+domain config -boot ${yocto_path}/sd_card
+domain config -image ${yocto_path}/sd_card
+domain config -sysroot ${sdk_path}/sysroots/aarch64-poky-linux
 platform generate
