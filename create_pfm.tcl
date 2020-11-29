5795c177b620e818825996de9e36fd405262f84f
Hidemi Ishihara <hidemi@aquaxis.com>
2020-11-29 10:49:08 +0900

    Update for 2020.2

--- create_pfm.tcl
+++ create_pfm.tcl
@@ -1,4 +1,4 @@
-set yocto_path "/home/hidemi/yocto/yocto_dunfell/build_ultra96v2"
+set yocto_path "/disk/yocto/yocto_gatesgarth/build_ultra96v2"
 set sdk_path   "/opt/poky/3.2_ultra96v2"
 
 platform create -name "ultra96v2_platform" -hw ./ultra96v2.xsa -out ./ -no-boot-bsp -proc psu_cortexa53 -os linux
@@ -7,5 +7,5 @@ domain active linux_domain
 domain config -bif ${yocto_path}/sd_card/build_vitis.bif
 domain config -boot ${yocto_path}/sd_card
 domain config -image ${yocto_path}/sd_card
-domain config -sysroot ${sdk_path}/sysroots/aarch64-poky-linux
+domain config -sysroot ${sdk_path}/sysroots/cortexa72-cortexa53-poky-linux
 platform generate
